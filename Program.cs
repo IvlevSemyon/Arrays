b49b67590188385ff43bfd2fe7ebba6763ffd26f
Ens1 <75061655+eNS1-ivlsem@users.noreply.github.com>
2023-03-23 01:55:22 +0300

    Arrays
    Arrays

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            var array = new int[5];
+
+            for ( int i = 0; i < array.Length; i++ )
+                array[i] = (i + 1) * (i + 1);
+
+            foreach(var item in array)
+                Console.WriteLine(item);
+        }
+    }
+}
