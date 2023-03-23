a2a59c085f731596f89ca65ce11a03b01bbf8449
Ens1 <75061655+eNS1-ivlsem@users.noreply.github.com>
2023-03-23 12:49:48 +0300

    EvenMethods
    AddEvenMethods

---
+++ EvenArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class EvenArray
+    {
+        public static int[] GetFirstEvenNumbers( int count )
+        {
+            int[] array = new int[count];
+            for ( int i = 0; i < count; i++ )
+            {
+                array[i] = ( i + 1 ) * 2;
+            }
+            return array;
+        }
+
+        public static int[] GetFirstEvenNumbersForOneLine( int count ) // я в абалдуе с этого метода решения)))
+        {
+            return Enumerable.Range( 1, count ).Select( x => x * 2 ).ToArray(); // до LINQ ещё не дошел и методов расширения
+        }
+    }
+}
---
+++ FirstStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class FirstStep
+    {
+        internal static void EvenArray( ref int[] array )
+        {
+            for ( int i = 0; i < array.Length; i++ )
+                array[i] = ( i + 1 ) * ( i + 1 );
+        }
+    }
+}
